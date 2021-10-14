d756e9e4433f3974f8dc824d0035fe1b8364559a
Linus Moreau <linus.moreau@gmail.com>
2021-10-13 21:20:33 -0700

    Change CourseList to directly extend ArrayList<Course>

--- src/main/model/CourseList.java
+++ src/main/model/CourseList.java
@@ -3,48 +3,33 @@ package model;
 import java.util.ArrayList;
 import java.util.Objects;
 
-public class CourseList {
-    private final ArrayList<Course> courseList;
+public class CourseList extends ArrayList<Course> {
 
     // MODIFIES: this
     // EFFECTS:  makes course list
     public CourseList() {
-        courseList = new ArrayList<>();
+        super();
     }
 
     // MODIFIES: this
     // EFFECTS:  adds course if no existing courses have the same name, otherwise throws exception
-    public void add(Course course) throws CourseAlreadyExists {
-        for (Course c : courseList) {
+    public boolean add(Course course) {
+        for (Course c : this) {
             if (Objects.equals(c.getName(), course.getName())) {
-                throw new CourseAlreadyExists();
+                return false;
             }
         }
-        courseList.add(course);
+        super.add(course);
+        return true;
     }
 
-    // REQUIRES: course is in the list
-    // MODIFIES: this
-    // EFFECTS:  removes course from the list
-    public void remove(Course course) {
-        courseList.remove(course);
-    }
-
-    // EFFECTS: returns the course with the given name
-    public Course find(String name) {
-        for (Course course : courseList) {
+    // EFFECTS: returns the course with the given name; if no corresponding course exists, returns null
+    public Course get(String name) {
+        for (Course course : this) {
             if (Objects.equals(course.getName(), name)) {
                 return course;
             }
         }
         return null;
-    }
-
-    public ArrayList<Course> getCourseList() {
-        return courseList;
-    }
-
-    public int size() {
-        return courseList.size();
     }
 }
