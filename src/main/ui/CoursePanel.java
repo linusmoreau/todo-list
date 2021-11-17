6f28c5ba0f6cea8f0d7c824320ed03c7e906e51b
Linus Moreau <linus.moreau@gmail.com>
2021-11-16 16:22:08 -0800

    Make format adjustments for CoursePanel

--- src/main/ui/CoursePanel.java
+++ src/main/ui/CoursePanel.java
@@ -11,36 +11,58 @@ public class CoursePanel extends JPanel {
     private ToDoList toDoList;
     private final JButton addButton;
     private final int width;
+    private static final int VGAP = 8;
+    private static final int HGAP = 16;
 
     public CoursePanel(ToDoList toDoList, int width) {
         this.width = width;
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         addButton = new JButton("Add");
         addButton.addActionListener(e -> addCourse());
+        addButton.setAlignmentX(CENTER_ALIGNMENT);
         updateCourses(toDoList);
     }
 
     public void updateCourses(ToDoList toDoList) {
         this.toDoList = toDoList;
         removeAll();
-        add(Box.createRigidArea(new Dimension(width, 16)));
+        add(Box.createRigidArea(new Dimension(width, VGAP)));
         add(addButton);
-        add(Box.createRigidArea(new Dimension(width, 16)));
+        add(Box.createRigidArea(new Dimension(width, VGAP)));
 
         for (Course course : toDoList.getCourses()) {
             add(makeCourseComponent(course));
-            add(Box.createRigidArea(new Dimension(width, 16)));
+            add(Box.createRigidArea(new Dimension(width, VGAP)));
         }
         updateUI();
     }
 
     private JComponent makeCourseComponent(Course course) {
         JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout(FlowLayout.LEADING, 8, 4));
+        panel.setLayout(new BorderLayout());
+        Color background = Color.lightGray;
+        JPanel coursePanel = makeCourseTextPanel(course);
+        JPanel buttonPanel = makeCourseButtonPanel(course);
+        coursePanel.setBackground(background);
+        buttonPanel.setBackground(background);
+        panel.add(coursePanel, BorderLayout.WEST);
+        panel.add(buttonPanel, BorderLayout.EAST);
+        panel.setMaximumSize(new Dimension(width, panel.getPreferredSize().height));
+        panel.setBackground(background);
+        return panel;
+    }
+
+    private JPanel makeCourseTextPanel(Course course) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout(FlowLayout.LEADING, HGAP, VGAP));
         panel.add(new JLabel(course.getName()));
         panel.add(new JLabel("Number of Assignments: " + course.getAssignments().size()));
         panel.add(new JLabel("Number of Exams: " + course.getExams().size()));
+        return panel;
+    }
 
+    private JPanel makeCourseButtonPanel(Course course) {
+        JPanel panel = new JPanel();
         JButton editButton = new JButton("Edit");
         editButton.addActionListener(e -> editCourse(course));
         panel.add(editButton);
@@ -48,9 +70,6 @@ public class CoursePanel extends JPanel {
         JButton deleteButton = new JButton("Delete");
         deleteButton.addActionListener(e -> deleteCourse(course));
         panel.add(deleteButton);
-
-        panel.setBackground(Color.lightGray);
-        panel.setMaximumSize(new Dimension(width, 64));
         return panel;
     }
 
